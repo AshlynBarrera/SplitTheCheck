5527a50c7e16df4a8fca43999cb470fa742a390b
DaySlacker <111376118+AshlynBarrera@users.noreply.github.com>
2023-04-11 09:19:52 -0500

    Initial Commit

---
+++ SplitTheCheck/SplitTheCheckApp.swift
@@ -0,0 +1,17 @@
+//
+//  SplitTheCheckApp.swift
+//  SplitTheCheck
+//
+//  Created by Ashlyn Barrera on 4/11/23.
+//
+
+import SwiftUI
+
+@main
+struct SplitTheCheckApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
